41168a1afc10b70395a48f9c3750b7a06878f737
Corbin <Corbinchristopher1@gmail.com>
2023-12-06 10:22:09 -0700

    Style post container

--- pages/blog.js
+++ pages/blog.js
@@ -16,7 +16,7 @@ export default function Blog() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          My Blog Posts        
+          Posts        
         </h1>
 
         <p className={styles.description}>
