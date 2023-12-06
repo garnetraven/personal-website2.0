d244e8d24b59ad278d0e5e228add8dae7e0c58b8
Corbin <Corbinchristopher1@gmail.com>
2023-12-06 10:35:51 -0700

    Add blog post feature

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,19 @@
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Navbar from 'components/Navbar'
 import Footer from 'components/Footer'
 import styles from '../styles/Blog.module.css'
 
 export default function Blog() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    // Fetch posts from local filesystem
+    fetch('/posts')
+      .then(response => response.json())
+      .then(data => setPosts(data))
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -19,9 +29,12 @@ export default function Blog() {
           Posts        
         </h1>
 
-        <p className={styles.description}>
-          Coming soon...
-        </p>
+        {posts.map(post =>(
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </div>
+        ))}
       </main>
 
       <Footer />
